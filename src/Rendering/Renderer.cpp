9c8298971cb8bb76b9bbe8390f658c6b4ebc611e
jbcolli2 <jbcolli2@gmail.com>
2023-03-30 23:24:09 -0400

    Copying over from other branch

--- src/Rendering/Renderer.cpp
+++ src/Rendering/Renderer.cpp
@@ -15,6 +15,137 @@
 
 using namespace seng;
 
+//***********************************************************
+//       Texture Quad
+//***********************************************************
+TextureQuad::TextureQuad()
+{
+    m_vertices = std::vector<Vert3x2f>{
+            Vert3x2f(-1.f, 1.0f, 0.0f, 0.f, 1.f),
+            Vert3x2f(-1.f, -1.f, 0.0f, 0.f, 0.f),
+            Vert3x2f(1.f, -1.f, 0.0f, 1.f, 0.f),
+
+            Vert3x2f(-1.f, 1.f, 0.0f, 0.f, 1.f),
+            Vert3x2f(1.f, -1.f, 0.0f, 1.f, 0.f),
+            Vert3x2f(1.f, 1.f, 0.0f, 1.f, 1.f)
+    };
+}
+
+void TextureQuad::StartUp()
+{
+    GenAndLoadVAO(m_vao, m_vbo, m_vertices);
+}
+
+TextureQuad::~TextureQuad()
+{
+    glDeleteVertexArrays(1, &m_vao);
+    glDeleteBuffers(1, &m_vbo);
+}
+
+TextureQuad::TextureQuad(TextureQuad&& other) : m_vao(other.m_vao), m_vbo(other.m_vbo),
+                                                m_vertices(other.m_vertices)
+{
+    other.m_vao = 0;
+    other.m_vbo = 0;
+}
+
+TextureQuad& TextureQuad::operator=(TextureQuad&& other)
+{
+    m_vao = other.m_vao;
+    m_vbo = other.m_vbo;
+    m_vertices = other.m_vertices;
+
+    other.m_vao = 0;
+    other.m_vbo = 0;
+
+    return *this;
+}
+
+void TextureQuad::Render(unsigned int tbo)
+{
+    glBindVertexArray(m_vao);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, tbo);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    glBindVertexArray(0);
+}
+
+
+//***********************************************************
+//       Framebuffer Code
+//***********************************************************
+FrameBufferFP::FrameBufferFP(int screenWidth, int screenHeight) : m_screenWidth(screenWidth),
+                                                                  m_screenHeight(screenHeight)
+{
+    glGenFramebuffers(1, &m_fbo);
+    glBindFramebuffer(GL_FRAMEBUFFER, m_fbo);
+
+    /////////////  Bind texture to FBO  ///////////////////
+    glGenTextures(1, &m_tbo);
+    glBindTexture(GL_TEXTURE_2D, m_tbo);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB16F, m_screenWidth, m_screenHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_tbo, 0);
+
+    glGenRenderbuffers(1, &m_rbo);
+    glBindRenderbuffer(GL_RENDERBUFFER, m_rbo);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, m_screenWidth, m_screenHeight);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, m_rbo);
+
+    /////////////  Check if complete  ///////////////////
+    if(glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+        std::cout << "Framebuffer NOT complete\n";
+
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
+FrameBufferFP::FrameBufferFP(FrameBufferFP&& other) :
+        m_fbo(other.m_fbo), m_rbo(other.m_rbo), m_tbo(other.m_tbo), m_screenWidth(other.m_screenWidth),
+        m_screenHeight(other.m_screenHeight)
+{
+    other.m_fbo = 0;
+    other.m_rbo = 0;
+    other.m_tbo = 0;
+}
+
+FrameBufferFP& FrameBufferFP::operator=(FrameBufferFP&& other)
+{
+    m_fbo = other.m_fbo;
+    m_rbo = other.m_rbo;
+    m_tbo = other.m_tbo;
+    m_screenWidth = other.m_screenWidth;
+    m_screenHeight = other.m_screenHeight;
+
+    other.m_fbo = 0;
+    other.m_rbo = 0;
+    other.m_tbo = 0;
+
+    return *this;
+}
+
+FrameBufferFP::~FrameBufferFP()
+{
+    glDeleteFramebuffers(1, &m_fbo);
+    glDeleteRenderbuffers(1, &m_rbo);
+    glDeleteTextures(1, &m_tbo);
+}
+
+
+void FrameBufferFP::Bind()
+{
+    glBindFramebuffer(GL_FRAMEBUFFER, m_fbo);
+}
+
+void FrameBufferFP::Release()
+{
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
+unsigned int FrameBufferFP::GetTBO()
+{
+    return m_tbo;
+}
 
 
 
